8d3c05e21ea8a330fdb5a86aa3a1c238d6b60059
Mose01 <dmosashvilil@gmail.com>
2026-03-27 10:53:44 +0400

    შეცვლილი სკრიფტი უფრო ახალზე

--- AD/UPN Change.ps1
+++ AD/UPN Change.ps1
@@ -1,13 +1,16 @@
-﻿$ENterDomain = Read-Host 'moa.gov.ge'
- 
-$routableDomain = $EnterDomain
+﻿# ==================== UPN შეცვლა ====================
 
-$users = Get-ADUser -Filter {UserPrincipalName -like '*'} -SearchBase "OU=MOA,OU=moa.gov.ge,OU=MoaAll,DC=moa,DC=gov,DC=ge"
-foreach ($user in $users) {
-    $userName = $user.UserPrincipalName.Split('@')[0]
-    $UPN = $userName + "@mepa.gov.ge" + $routableDomain
-    
-    Write-Host $user.Name $user.UserPrincipalName $UPN
+$Old = "moa.gov.ge"     # ძველი დომენი
+$New = "mepa.gov.ge"    # ახალი დომენი
 
-    $user | Set-ADUser -UserPrincipalName $UPN
+$WhatIf = $true         # შეცვალე $false-ზე, როცა რეალურად გინდა ცვლილება
+
+# ===================================================
+
+Get-ADUser -Filter * -SearchBase "OU=MOA,OU=moa.gov.ge,OU=MoaAll,DC=moa,DC=gov,DC=ge" | 
+ForEach-Object { 
+    $newUPN = $_.UserPrincipalName.Replace($Old, $New)
+    Set-ADUser $_ -UserPrincipalName $newUPN -WhatIf:$WhatIf
 }
+
+Write-Host "სკრიპტი დასრულდა. რეჟიმი: $(if($WhatIf){"ტესტი"} else {"რეალური ცვლილება"})" -ForegroundColor $(if($WhatIf){"Yellow"} else {"Green"})
