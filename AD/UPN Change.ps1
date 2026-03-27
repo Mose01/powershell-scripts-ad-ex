f98f0708a06e93457ff93a016386c1059ec776bc
Mose01 <dmosashvilil@gmail.com>
2026-03-27 10:25:25 +0400

    add UPN Change file

---
+++ AD/UPN Change.ps1
@@ -0,0 +1,13 @@
+﻿$ENterDomain = Read-Host 'moa.gov.ge'
+ 
+$routableDomain = $EnterDomain
+
+$users = Get-ADUser -Filter {UserPrincipalName -like '*'} -SearchBase "OU=MOA,OU=moa.gov.ge,OU=MoaAll,DC=moa,DC=gov,DC=ge"
+foreach ($user in $users) {
+    $userName = $user.UserPrincipalName.Split('@')[0]
+    $UPN = $userName + "@mepa.gov.ge" + $routableDomain
+    
+    Write-Host $user.Name $user.UserPrincipalName $UPN
+
+    $user | Set-ADUser -UserPrincipalName $UPN
+}
