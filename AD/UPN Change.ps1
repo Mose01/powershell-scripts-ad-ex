f338dc9afe7867c8869b186cd3b3ba5ef5e45994
Mose01 <dmosashvilil@gmail.com>
2026-03-27 11:15:17 +0400

    გადავაკეთე winelab.gov.ge-თვის

--- AD/UPN Change.ps1
+++ AD/UPN Change.ps1
@@ -1,13 +1,13 @@
 ﻿# ==================== UPN შეცვლა ====================
 
-$Old = "moa.gov.ge"     # ძველი დომენი
-$New = "mepa.gov.ge"    # ახალი დომენი
+$Old = "winelab.org.ge"     # ძველი დომენი
+$New = "winelab.gov.ge"    # ახალი დომენი
 
 $WhatIf = $true         # შეცვალე $false-ზე, როცა რეალურად გინდა ცვლილება
 
 # ===================================================
 
-Get-ADUser -Filter * -SearchBase "OU=MOA,OU=moa.gov.ge,OU=MoaAll,DC=moa,DC=gov,DC=ge" | 
+Get-ADUser -Filter * -SearchBase "OU=WINELAB.GOV.GE,OU=WINELAB.GOV.GE,OU=MoaAll,DC=moa,DC=gov,DC=ge" | 
 ForEach-Object { 
     $newUPN = $_.UserPrincipalName.Replace($Old, $New)
     Set-ADUser $_ -UserPrincipalName $newUPN -WhatIf:$WhatIf
